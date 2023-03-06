bf4f8ca9b601e3f0aaa96db017e33e20d5fdb847
Vitalii <number1pyak@gmail.com>
2023-03-06 17:58:40 +0700

    -aHomework 31

--- js/script.js
+++ js/script.js
@@ -1,59 +1,36 @@
-'use strict';
+'use strict'
+window.addEventListener('DOMContentLoaded',() => {
 
-//Привяжите всем инпутам следующее событие - по потери фокуса каждый 
-//инпут выводит свое value в абзац с id="test"
-
-// let elems = document.querySelectorAll('input');
-// let test = document.getElementById('test');
-// for (let i = 0; i < elems.length; i++) {
-//     elems[i].addEventListener('blur', ()=> 
-//     test.innerHTML = `${elems[i].value}`
-//     );
-// }
-
-
-// Для всех инпутов сделайте так, чтобы они выводили свой value алертом 
-// при нажатии на любой из них, но только по первому нажатию. Повторное 
-// нажатие на инпут не должно вызывать реакции.
-
-// let elems = document.querySelectorAll('input');
-
-// for (let i = 0; i < elems.length; i++) {
-//     const alertValue = () => {
-//         alert(elems[i].value);
-//         elems[i].removeEventListener('click', alertValue)
-//     }
-//     elems[i].addEventListener('click', alertValue);
-   
-// }
-
-
-// Даны абзацы с числами. По нажатию на абзац в нем должен появится 
-//квадрат числа, которое он содержит.
-
-// let elems = document.querySelectorAll('p');
-// elems.forEach(elem => {
-//     elem.addEventListener('click', () => {
-//         elem.innerHTML = `${+elem.textContent * +elem.textContent} `;
-//     });
-// })
-
-// Даны инпуты. Сделайте так, чтобы все инпуты по потери фокуса проверяли 
-//свое содержимое на правильное количество символов. Сколько символов 
-//должно быть в инпуте, указывается в атрибуте data-length. Если вбито 
-//правильное количество, то граница инпута становится зеленой, если 
-//неправильное - красной.
-
-    // let elems = document.querySelectorAll('input');
-    // elems.forEach(elem => {
-    //     elem.addEventListener('blur', () => {
-    //         if(elem.value.length == elem.dataset.length) {
-    //             elem.style.borderColor = 'green';
-    //         }else{
-    //             elem.style.borderColor = 'red';
-
-    //         }
-    //     })
-    // })
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
     
-       
+    function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.style.display = "none";
+        });
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    } 
+
+    function showTabsContent(i = 0) {
+        tabsContent[i].style.display ='block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabsContent();
+    showTabsContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                } 
+            });
+        }
+    });
+});
